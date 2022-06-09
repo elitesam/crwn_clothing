c2b413b126821c36f3b7363e1326981c71e03108
elitesam <sahusambit6@gmail.com>
2022-06-09 17:37:46 +0530

    added redux-thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,5 @@
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { useParams } from 'react-router-dom';
 
@@ -6,13 +7,14 @@ import { useEffect, useState, Fragment } from 'react';
 
 import { useSelector } from 'react-redux';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
 import './category.styles.scss';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
   
 
@@ -23,11 +25,16 @@ const Category = () => {
   return (
     <Fragment>
       <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
-        {products &&
-          products.map((product) => <ProductCard key={product.id} product={product}/>)
-        }
-      </div>
+      {
+        isLoading
+          ? <Spinner/>
+          : <div className='category-container'>
+              {products &&
+                products.map((product) => <ProductCard key={product.id} product={product}/>)
+              }
+            </div>
+      }
+      
     </Fragment>
   )
     
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,11 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
-
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 
 import './shop.styles.scss';
@@ -15,12 +13,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
